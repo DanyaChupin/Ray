f801104adf41d1a81ffd339cd6e095452665fbb0
DanyaChupin <chupa.chupa.14@mail.ru>
2024-05-15 15:52:14 +0300

    not-found страница

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,13 @@
+import { Logo } from '@/components/Logo/Logo'
+import styles from './NotFound.module.scss'
+import { BackLink } from '@/components/backLink/BackLink'
+
 export default function NotFound() {
-	return <div>not-found</div>
+	return (
+		<div className={styles['notFound']}>
+			<Logo />
+			<p className={styles['description']}>Страница не найдена </p>
+			<BackLink returnBackUrl="/" />
+		</div>
+	)
 }

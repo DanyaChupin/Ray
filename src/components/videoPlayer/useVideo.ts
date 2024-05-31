11ec0d2c91be6bc226d9bf9c8fb75556222b284e
DanyaChupin <chupa.chupa.14@mail.ru>
2024-05-31 19:49:34 +0300

    fix

--- src/components/videoPlayer/useVideo.ts
+++ src/components/videoPlayer/useVideo.ts
@@ -63,6 +63,7 @@ export const useVideo = (autoPlay: boolean, videoId: string) => {
 			video.pause()
 			handleMove(videoId)
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	const fastForward = useCallback(() => {

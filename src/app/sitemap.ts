265f451e04736befbcd0c19f0de7557c4224103d
DanyaChupin <chupa.chupa.14@mail.ru>
2024-06-02 00:32:49 +0300

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,39 +1,6 @@
-import { getFilms } from '@/config/api.config'
-import { IVideo } from '@/shared/types/video.type'
 import { MetadataRoute } from 'next'
-export async function generateSitemap() {
-	try {
-		const response = await fetch(
-			process.env.NEXT_PUBLIC_BASE_API + getFilms('1'),
-			{
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN_KEY}`,
-				},
-			}
-		)
 
-		const res: { data: IVideo[] } = await response.json()
-		const filmEntries: MetadataRoute.Sitemap = res.data.map((video) => ({
-			url: `${process.env.NEXT_PUBLIC_BASE_URL}/catalog/${video.videoId}`,
-			lastModified: new Date(video.createdAt),
-		}))
-		return filmEntries
-	} catch (error) {
-		return [
-			{
-				url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-			},
-			{
-				url: `${process.env.NEXT_PUBLIC_BASE_URL}/catalog`,
-				lastModified: new Date(),
-			},
-		]
-	}
-}
-export default async function sitemap(
-	filmEntries: { url: string; lastModified: Date }[]
-): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	return [
 		{
 			url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
@@ -42,6 +9,5 @@ export default async function sitemap(
 			url: `${process.env.NEXT_PUBLIC_BASE_URL}/catalog`,
 			lastModified: new Date(),
 		},
-		...filmEntries,
 	]
 }

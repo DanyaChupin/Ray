37432ccf3cdf479d45932282c3617a7eb2206db8
DanyaChupin <chupa.chupa.14@mail.ru>
2024-06-04 20:02:52 +0300

    test fix 7

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,5 @@ export default createMiddleware({
 })
 
 export const config = {
-	matcher: ['/', '/catalog/:path*', '/([\\w-]+)?/catalog/([\\w-]+)'],
+	matcher: ['/', '/catalog/:videoId', '/([\\w-]+)?/catalog/([\\w-]+)'],
 }

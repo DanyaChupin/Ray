429646c5543173ca43ae0ae27e53f2186a06628f
DanyaChupin <chupa.chupa.14@mail.ru>
2024-06-04 19:18:22 +0300

    test fix 23

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,24 @@
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
+// import { getRequestConfig } from 'next-intl/server'
+
+// const locales = ['en', 'ru']
+
+// export default getRequestConfig(async ({ locale }) => {
+// 	if (!locales.includes(locale as any)) notFound()
+
+// 	return {
+// 		messages: (await import(`../messages/${locale}.json`)).default,
+// 	}
+// })
 import { getRequestConfig } from 'next-intl/server'
 
-const locales = ['en', 'ru']
-
-export default getRequestConfig(async ({ locale }) => {
-	if (!locales.includes(locale as any)) notFound()
+export default getRequestConfig(async ({ locale = 'ru' }) => {
+	// Provide a static locale, fetch a user setting,
+	// read from `cookies()`, `headers()`, etc.
+	// const locales = ['en','ru'];
 
 	return {
+		locale,
 		messages: (await import(`../messages/${locale}.json`)).default,
 	}
 })

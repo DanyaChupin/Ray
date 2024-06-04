c0449c495df7bce55907865b8460de6f2b1c5617
DanyaChupin <chupa.chupa.14@mail.ru>
2024-06-04 19:36:21 +0300

    test fix 5

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,5 @@ export default createMiddleware({
 })
 
 export const config = {
-	matcher: [
-		'/',
-		'/catalog/:path*',
-		'/[^/]+(?:/.*)?$', // измененный шаблон
-		'/([w-]+)?/catalog/(.+)',
-	],
+	matcher: ['/', '/catalog/:path*', '/([w-]+)?/catalog/(.+)'],
 }

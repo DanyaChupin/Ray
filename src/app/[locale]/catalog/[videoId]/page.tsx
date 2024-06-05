ef95735e8d29dc7160f02ec6dc02b8227ecb39a8
DanyaChupin <chupa.chupa.14@mail.ru>
2024-06-05 12:14:04 +0300

    test fix 12

--- src/app/[locale]/catalog/[videoId]/page.tsx
+++ src/app/[locale]/catalog/[videoId]/page.tsx
@@ -1,9 +1,4 @@
-import axiosClassic from '@/api/interceptors'
 import Film from '@/components/film/FIlm'
-import { getFilmById } from '@/config/api.config'
-import { IVideo } from '@/shared/types/video.type'
-import { Metadata } from 'next'
-import { redirect } from 'next/navigation'
 
 export default function FilmPage({
 	params: { videoId },
@@ -16,26 +11,26 @@ export default function FilmPage({
 		</>
 	)
 }
-export async function generateMetadata({
-	params: { videoId },
-}: {
-	params: { videoId: string }
-}): Promise<Metadata> {
-	try {
-		const response = await axiosClassic.get<IVideo>(getFilmById(videoId))
+// export async function generateMetadata({
+// 	params: { videoId },
+// }: {
+// 	params: { videoId: string }
+// }): Promise<Metadata> {
+// 	try {
+// 		const response = await axiosClassic.get<IVideo>(getFilmById(videoId))
 
-		return {
-			title: response.data.name,
-			description: response.data.description || '',
-			openGraph: {
-				images: [
-					{
-						url: response.data.screenshots[0],
-					},
-				],
-			},
-		}
-	} catch (error) {
-		redirect('/catalog')
-	}
-}
+// 		return {
+// 			title: response.data.name,
+// 			description: response.data.description || '',
+// 			openGraph: {
+// 				images: [
+// 					{
+// 						url: response.data.screenshots[0],
+// 					},
+// 				],
+// 			},
+// 		}
+// 	} catch (error) {
+// 		redirect('/catalog')
+// 	}
+// }
